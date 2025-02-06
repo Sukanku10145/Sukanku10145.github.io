f5f9a01e32444f01c951a7e0cc12e70ea656d6bc
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-02-06 13:49:32 +0100

    06/02/2025

--- footer.js
+++ footer.js
@@ -1,5 +1,21 @@
 document.write(`
-    <footer>
-        <p>Contacto: email@ejemplo.com | Tel: 123-456-789</p>
-    </footer>
+<p><h3>Redes Sociales</h3></p>
+<div class="row g-3">
+  <div class="col" id="links1">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://es-es.facebook.com/people/Skunks/100090142473339/">
+                <i class="bi bi-link-45deg"></i>Facebook
+            </a><br>
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/zooarcade/?hl=en">
+                <i class="bi bi-link-45deg"></i>Instagram
+            </a>
+  </div>
+  <div class="col" id="links2">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="http://www.youtube.com/@Zooarcade">
+                <i class="bi bi-link-45deg"></i>YouTube
+            </a><br>
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@zooarcade?lang=en">
+                <i class="bi bi-link-45deg"></i>TikTok
+            </a>
+  </div>
+</div>
 `);
