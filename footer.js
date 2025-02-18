811874c0aa671388c5f2e009ad81aad8df81c49f
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-02-18 10:26:46 +0100

    18/02/2025

--- footer.js
+++ footer.js
@@ -2,18 +2,18 @@ document.write(`
 <p><h3>Redes Sociales</h3></p>
 <div class="row g-3">
   <div class="col" id="links1">
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://es-es.facebook.com/people/Skunks/100090142473339/">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://es-es.facebook.com/people/Skunks/100090142473339/" target="_blank">
                 <i class="bi bi-link-45deg"></i>Facebook
             </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/musicmultitool/">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/musicmultitool/" target="_blank">
                 <i class="bi bi-link-45deg"></i>Instagram
             </a>
   </div>
   <div class="col" id="links2">
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.youtube.com/@MusicMultitool">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.youtube.com/@MusicMultitool" target="_blank">
                 <i class="bi bi-link-45deg"></i>YouTube
             </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@musicmultitool">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@musicmultitool" target="_blank">
                 <i class="bi bi-link-45deg"></i>TikTok
             </a>
   </div>
