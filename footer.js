b533897e62809cab67ce2b3a6ce7a6eb4e9caa2b
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-02-18 10:23:43 +0100

    18/02/2025

--- footer.js
+++ footer.js
@@ -5,15 +5,15 @@ document.write(`
             <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://es-es.facebook.com/people/Skunks/100090142473339/">
                 <i class="bi bi-link-45deg"></i>Facebook
             </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/zooarcade/?hl=en">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/musicmultitool/">
                 <i class="bi bi-link-45deg"></i>Instagram
             </a>
   </div>
   <div class="col" id="links2">
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="http://www.youtube.com/@Zooarcade">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.youtube.com/@MusicMultitool">
                 <i class="bi bi-link-45deg"></i>YouTube
             </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@zooarcade?lang=en">
+            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@musicmultitool">
                 <i class="bi bi-link-45deg"></i>TikTok
             </a>
   </div>
