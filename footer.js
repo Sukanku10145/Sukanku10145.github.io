66205ef403a897641df9260bd3d83476e2311427
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-02-20 12:27:41 +0100

    Hay que terminar las imagenes del psd

--- footer.js
+++ footer.js
@@ -1,21 +1,22 @@
 document.write(`
-<p><h3>Redes Sociales</h3></p>
-<div class="row g-3">
-  <div class="col" id="links1">
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://es-es.facebook.com/people/Skunks/100090142473339/" target="_blank">
-                <i class="bi bi-link-45deg"></i>Facebook
-            </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.instagram.com/musicmultitool/" target="_blank">
-                <i class="bi bi-link-45deg"></i>Instagram
-            </a>
-  </div>
-  <div class="col" id="links2">
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.youtube.com/@MusicMultitool" target="_blank">
-                <i class="bi bi-link-45deg"></i>YouTube
-            </a><br>
-            <a id="redes" class="icon-link icon-link-hover" 25, 135, 84;" href="https://www.tiktok.com/@musicmultitool" target="_blank">
-                <i class="bi bi-link-45deg"></i>TikTok
-            </a>
-  </div>
-</div>
-`);
+    <footer class="bg-dark text-white py-4">
+      <div class="container text-center">
+        <h5>Redes Sociales</h5>
+        <div class="d-flex justify-content-center gap-4">
+          <a class="text-white" href="https://es-es.facebook.com/people/Skunks/100090142473339/" target="_blank">
+            <i class="bi bi-facebook"></i> Facebook
+          </a>
+          <a class="text-white" href="https://www.instagram.com/musicmultitool/" target="_blank">
+            <i class="bi bi-instagram"></i> Instagram
+          </a>
+          <a class="text-white" href="https://www.youtube.com/@MusicMultitool" target="_blank">
+            <i class="bi bi-youtube"></i> YouTube
+          </a>
+          <a class="text-white" href="https://www.tiktok.com/@musicmultitool" target="_blank">
+            <i class="bi bi-tiktok"></i> TikTok
+          </a>
+        </div>
+      </div>
+    </footer>
+    `);
+    
--- navigation.js
+++ navigation.js
@@ -1,13 +1,25 @@
 document.write(`
-  <ul class="nav nav-tabs" id="navigation">
-    <li class="nav-item">
-      <a class="nav-link" href="index.html">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="about.html">About</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="contact.html">Contact</a>
-    </li>
-  </ul>
-`);
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html">MusicMultitool</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  `);
+  
