36ae4a8b3bdd14f8a21222785a746dc4fde81f90
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-03-24 11:17:03 +0100

    Base con chatgpt

--- nav.js
+++ nav.js
@@ -1,16 +1,17 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+// Este script inserta el menú de navegación en la página
+document.querySelector('nav').innerHTML = `
     <div class="container">
-        <a class="navbar-brand" href="index.php">Inicio</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="historia.php">Nuestra Historia</a></li>
-                <li class="nav-item"><a class="nav-link" href="galeria.php">Galería</a></li>
-                <li class="nav-item"><a class="nav-link" href="anecdotas.php">Anécdotas</a></li>
-                <li class="nav-item"><a class="nav-link" href="extras.php">Extras</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.html?access=plazadeespana07/12/2024">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link" href="historia.html">Nuestra Historia</a></li>
+                <li class="nav-item"><a class="nav-link" href="galeria.html">Galería</a></li>
+                <li class="nav-item"><a class="nav-link" href="anecdotas.html">Anécdotas</a></li>
+                <li class="nav-item"><a class="nav-link" href="extras.html">Extras</a></li>
             </ul>
         </div>
     </div>
-</nav>
+`;
