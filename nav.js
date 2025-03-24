c45818a308617d524caf72dae2b3e5206faa1261
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-03-24 13:37:14 +0100

    arreglo nav

--- nav.js
+++ nav.js
@@ -1,30 +1,39 @@
+// nav.js
 document.querySelector('nav').innerHTML = `
-<div class="container">
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul class="navbar-nav gap-3">
+<div class="container-fluid">
+    <button class="navbar-toggler" type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav justify-content-center w-100 gap-2">
             <li class="nav-item">
-                <a class="nav-link btn btn-rosa d-flex align-items-center gap-2" 
+                <a class="nav-link btn btn-rosa" 
                    href="index.html?access=plazadeespana07/12/2024">
                     <i class="fas fa-home"></i> Inicio
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link btn btn-rosa d-flex align-items-center gap-2" href="historia.html">
+                <a class="nav-link btn btn-rosa" href="historia.html">
                     <i class="fas fa-heart"></i> Historia
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link btn btn-rosa d-flex align-items-center gap-2" href="galeria.html">
+                <a class="nav-link btn btn-rosa" href="galeria.html">
                     <i class="fas fa-images"></i> Galería
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link btn btn-rosa d-flex align-items-center gap-2" href="anecdotas.html">
+                <a class="nav-link btn btn-rosa" href="anecdotas.html">
                     <i class="fas fa-laugh-beam"></i> Anécdotas
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link btn btn-rosa d-flex align-items-center gap-2" href="extras.html">
+                <a class="nav-link btn btn-rosa" href="extras.html">
                     <i class="fas fa-gift"></i> Extras
                 </a>
             </li>
